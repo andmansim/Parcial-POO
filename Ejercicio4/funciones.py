99c1a4e5e9031fb8ee95c16e4bfaef9028f452e3
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 10:02:16 +0100

    amplio main

--- Ejercicio4/funciones.py
+++ Ejercicio4/funciones.py
@@ -24,34 +24,7 @@ class Cuenta:
             self.saldo = self.saldo - trans1
             return self.saldo      
  
-#Información del usuario       
-print("¿Cuál es su ID?")
-id = input()
-print("Nombre del titular:")
-nom = input()
-print("Fecha:")
-fe = input()
-print("Número de la cuenta:")
-num = input()
-print("Saldo de la cuenta:")
-sal = float(input())
 
-cuenta = Cuenta (id, nom, fe, num, sal)
 
-#Operaciones en la cuenta
-print("¿Quieres hacer alguna operación? transferir/retirar")
-usuario = input()
-if usuario == "retirar":
-    print("¿Cuánto dinero quiere retirar?")
-    ret = float(input())
-    print(cuenta.retirar(ret))
-elif usuario == "transferir":
-    print("¿Cuánto dinero quiere transferir?")
-    trans = float(input())
-    print("¿A quién lo quiere transferir?")
-    usuario = input()
-    print(cuenta.transferir(trans))
-else:
-    raise Exception("Esa operación no está disponible")
-
+    
 
--- Ejercicio4/main.py
+++ Ejercicio4/main.py
@@ -18,3 +18,19 @@ if __name__ == "__main__":
     sal = input()
     sal = float(sal)
     cuenta = funciones.Cuenta (id, nom, fe, num, sal)
+    
+    #Operaciones en la cuenta
+    print("¿Quieres hacer alguna operación? transferir/retirar")
+    usuario = input()
+    if usuario == "retirar":
+        print("¿Cuánto dinero quiere retirar?")
+        ret = float(input())
+        print(cuenta.retirar(ret))
+    elif usuario == "transferir":
+        print("¿Cuánto dinero quiere transferir?")
+        trans = float(input())
+        print("¿A quién lo quiere transferir?")
+        usuario = input()
+        print(cuenta.transferir(trans))
+    else:
+        raise Exception("Esa operación no está disponible")
