28e3eaf5b23fb3c9ec60ce74728f32ba086c0566
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 10:01:04 +0100

    cambio el main

--- Ejercicio4/main.py
+++ Ejercicio4/main.py
@@ -4,37 +4,17 @@ if __name__ == "__main__":
     #Información del usuario       
     print("¿Cuál es su ID?")
     id = input()
-    if id == str:
-        pass
-    else: 
-        raise Exception("Ese no es un ID válido")
+ 
     print("Nombre del titular:")
-    
     nom = input()
-    if nom == str:
-        pass
-    else: 
-        raise Exception("Ese no es un nombre válido")
-    
+
     print("Fecha:")
     fe = input()
-    if fe == str:
-        pass
-    else: 
-        raise Exception("Ese no es un fecha válida")
     
     print("Número de la cuenta:")
     num = input()
-    if num == int:
-        pass
-    else: 
-        raise Exception("Ese no es un número válido")
-    
+     
     print("Saldo de la cuenta:")
     sal = input()
-    if sal == int:
-        sal = float(sal)
-    else: 
-        raise Exception("Ese no es un saldo válido")
-
+    sal = float(sal)
     cuenta = funciones.Cuenta (id, nom, fe, num, sal)
