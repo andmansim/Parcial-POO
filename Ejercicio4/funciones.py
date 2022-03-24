7c98ed781cf5cb4d78872827eae759af868c2f01
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 08:40:58 +0100

    constructor del libro

---
+++ Ejercicio4/funciones.py
@@ -0,0 +1,7 @@
+class Libro:
+    def __init__(self, nombre, autor, titulo, isbn, genero ):
+        self.nombre = nombre
+        self.autor = autor
+        self.titulo = titulo
+        self.isbn = isbn
+        self.genero = genero
