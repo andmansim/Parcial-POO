9a48918bbea3afbc6ce67b20e6ab29ced3bac762
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 09:31:55 +0100

    clase cuenta

---
+++ Ejercicio4/funciones.py
@@ -0,0 +1,9 @@
+class Cuenta:
+    def __init__(self, ID, nombre, fecha, numero, saldo):
+        self.ID = ID
+        self.nombre = nombre
+        self.fecha = fecha
+        self.numero = numero
+        self.saldo = saldo
+        
+print()
