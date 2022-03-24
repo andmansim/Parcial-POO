0b68ab6dc80917a4319f62233e8b1b923ed5039c
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 10:16:16 +0100

    preguntamos cuenta

--- Ejercicio4/main.py
+++ Ejercicio4/main.py
@@ -2,6 +2,7 @@ import funciones
 import vip
 
 if __name__ == "__main__":
+
     #Información del usuario       
     print("¿Cuál es su ID?")
     id = input()
@@ -18,23 +19,45 @@ if __name__ == "__main__":
     print("Saldo de la cuenta:")
     sal = input()
     sal = float(sal)
-    cuenta = funciones.Cuenta (id, nom, fe, num, sal)
     
-    #Operaciones en la cuenta
-    print("¿Quieres hacer alguna operación? transferir/retirar")
-    usuario = input()
-    if usuario == "retirar":
-        print("¿Cuánto dinero quiere retirar?")
-        ret = float(input())
-        print(cuenta.retirar(ret))
-    elif usuario == "transferir":
-        print("¿Cuánto dinero quiere transferir?")
-        trans = float(input())
-        print("¿A quién lo quiere transferir?")
+    print("¿Qué tipo de cuenta tiene? corriente/vip")
+    cuenta = input()
+    if cuenta == "corriente":
+        cuenta = funciones.Cuenta (id, nom, fe, num, sal)
+    
+        #Operaciones en la cuenta
+        print("¿Quieres hacer alguna operación? transferir/retirar")
         usuario = input()
-        print(cuenta.transferir(trans))
-    else:
-        raise Exception("Esa operación no está disponible")
+        if usuario == "retirar":
+            print("¿Cuánto dinero quiere retirar?")
+            ret = float(input())
+            print(cuenta.retirar(ret))
+        elif usuario == "transferir":
+            print("¿Cuánto dinero quiere transferir?")
+            trans = float(input())
+            print("¿A quién lo quiere transferir?")
+            usuario = input()
+            print(cuenta.transferir(trans))
+        else:
+            raise Exception("Esa operación no está disponible")
+    elif cuenta == "vip":
 
-    #Cuenta Vip
-    v = vip.Vip(id, nom, fe, num, sal,-1000)
+        #Cuenta Vip
+        v = vip.Vip(id, nom, fe, num, sal,-1000)
+        #Operaciones en la cuenta
+        print("¿Quieres hacer alguna operación? transferir/retirar")
+        usuario = input()
+        if usuario == "retirar":
+            print("¿Cuánto dinero quiere retirar?")
+            ret = float(input())
+            print(cuenta.retirar(ret))
+        elif usuario == "transferir":
+            print("¿Cuánto dinero quiere transferir?")
+            trans = float(input())
+            print("¿A quién lo quiere transferir?")
+            usuario = input()
+            print(cuenta.transferir(trans))
+        else:
+            raise Exception("Esa operación no está disponible")
+    else:
+        raise Exception("Esa no es una opción válida")
--- Ejercicio4/vip.py
+++ Ejercicio4/vip.py
@@ -1,7 +1,30 @@
 import funciones
-class Vip(funciones.Cuenta):
+class Vip:
+        
     def __init__(self, ID, nombre, fecha, numero, saldo, saldo_negativo):
-        super().__init__(ID, nombre, fecha, numero, saldo)
+        self.ID = ID
+        self.nombre = nombre
+        self.fecha = fecha
+        self.numero = numero
+        self.saldo = saldo
         self.saldo_negarivo = saldo_negativo
+        
+    def retirar(self, ret1):
+
+        if ret1 >= self.saldo:
+            print("No puede retirar esta cantidad de dinero")
+        else:
+            print("Has retirado esta cantidad de dinero " + str(ret1) + " el día " + str(self.fecha))
+            self.saldo = self.saldo - ret1
+            return self.saldo
+    
+    def transferir(self, trans1):
+        if trans1 >= self.saldo:
+            print("No puede transferir esta cantidad de dinero")
+        else:
+
+            print("Has transferido esta cantidad de dinero " + str(trans1) + " el día " + str(self.fecha))
+            self.saldo = self.saldo - trans1
+            return self.saldo  
         
         
