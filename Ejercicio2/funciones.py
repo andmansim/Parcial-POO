5a9726b8e569c2088783f6bb51d1b093aac20899
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 09:05:08 +0100

    método descripción en la clase gato y pollo, y objetos

--- Ejercicio2/funciones.py
+++ Ejercicio2/funciones.py
@@ -16,4 +16,22 @@ class Gato(Mamifero):
     def __init__(self, especie, tipo, nombre):
         Mamifero.__init__(especie, tipo)
         self.nombre = nombre
+    def descripcion(self):
+        print("Especie: " + self.especie)
+        print("Clasificación: " + self.tipo)
+        print("Nombre: " + self.nombre)
+
+class Pollo (Oviparo):
+    def __init__(self, especie, tipo, nombre):
+        Oviparo.__init__(especie, tipo)
+        self.nombre = nombre
+    def descripcion(self):
+        print("Especie: " + self.especie)
+        print("Clasificación: " + self.tipo)
+        print("Nombre: " + self.nombre)
         
+pollo = Pollo("animal", "oviparo", "pollo")
+pollo.descripcion
+
+gato = Gato("animal", "mamifero", "gato")
+gato.descripcion
