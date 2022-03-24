7aa9758f4fd923250c6d3c5ace51897144db7798
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 08:48:35 +0100

    método descripcion

--- Ejercicio4/funciones.py
+++ Ejercicio4/funciones.py
@@ -4,7 +4,11 @@ class Libro:
         self.titulo = titulo
         self.isbn = isbn
         self.genero = genero
-
+    def descripcion(self):
+        print("El nombre del libro es " + self.titulo + " y es del género " + self.genero)
+        print("Su autor es " + self.autor)
+        print("Su ISBN es " + self.isbn )
+        
 print("Elija el nombre del libro a buscar:")
 tit = input()
 print("¿Quién es el autor del libro?")
@@ -15,3 +19,5 @@ print("¿Cuál es el isbn?")
 isbn = input()
 
 libro = Libro(tit, aut, isbn, gen)
+
+print(libro)
