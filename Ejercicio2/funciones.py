a052bed9f445ab818258b5a8022ef4359db9e1ee
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 08:59:46 +0100

    clase gato y cambio nombre a especie

--- Ejercicio2/funciones.py
+++ Ejercicio2/funciones.py
@@ -1,13 +1,19 @@
 class Animal:
-    def __init__(self, nombre):
-        self.nombre = nombre
+    def __init__(self, especie):
+        self.especie = especie
         
 class Mamifero(Animal):
-    def __init__(self, nombre, tipo):
-        Animal.__init__(nombre)
+    def __init__(self, especie, tipo):
+        Animal.__init__(especie)
         self.tipo = tipo
 
 class Oviparo(Animal):
-    def __init__(self, nombre, tipo):
-        Animal.__init__(nombre)
+    def __init__(self, especie, tipo):
+        Animal.__init__(especie)
         self.tipo = tipo
+    
+class Gato(Mamifero):
+    def __init__(self, especie, tipo, nombre):
+        Mamifero.__init__(especie, tipo)
+        self.nombre = nombre
+        
