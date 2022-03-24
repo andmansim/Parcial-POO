628c1674734dde7a53e99bb2a6be9fedd8e41424
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-24 09:39:57 +0100

    función retirar

--- Ejercicio4/funciones.py
+++ Ejercicio4/funciones.py
@@ -5,7 +5,15 @@ class Cuenta:
         self.fecha = fecha
         self.numero = numero
         self.saldo = saldo
-        
+    def retirar(self):
+        print("¿Cuánto dinero quiere retirar?")
+        ret = float(input())
+        if ret >= self.saldo:
+            print("No puede retirar esta cantidad de dinero")
+        else:
+            print("Has retirado esta cantidad de dinero " + ret + " este día " + self.fecha)
+            self.saldo = self.saldo - ret
+            return self.saldo
 print("¿Cuál es su ID?")
 id = input()
 print("Nombre del titular:")
@@ -18,5 +26,5 @@ print("Saldo de la cuenta:")
 sal = input()
 
 cuenta = Cuenta (id, nom, fe, num, sal)
-
+cuenta.retirar
 
